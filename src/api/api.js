2f3db80e8fcea32ce001141f67a0360a3d6cdedf
Punu N Gowda <115967261+ManyathaP@users.noreply.github.com>
2024-07-16 21:51:49 +0530

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = "http://localhost:8090"; // Replace with your actual IP address
+// const API_BASE_URL = "http://localhost:8090"; // Replace with your actual IP address
+
+const API_BASE_URL = "https://electricecoedge-spring-boot-production.up.railway.app/";
 
 export default API_BASE_URL;
